7171d7ae0638f4a89facb2e7366682ce4eea26da
Thiago de Souza <thiagodesouzarb@gmail.com>
2023-11-21 19:39:18 -0300

    Exercicio com matriz

---
+++ MatrizExercicio/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatrizExercicio
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[,] mat = new int[n, n];
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] values = Console.ReadLine().Split(' ');
+                for(int j = 0; j < values.Length; j++)
+                {
+                    mat[i,j] = int.Parse(values[j]);
+                }
+            }
+            Console.WriteLine("Main diagonal: ");
+            for (int i = 0; i < n;i++)
+            {
+                Console.WriteLine(mat[i, i] + " ");
+            }
+        }
+    }
+}
